06c058070a762d4e33d70330a3d83e45b5d84749
Eesha Ulhaq <eesha@Eeshas-Air.cogeco.local>
2022-05-29 00:28:24 -0400

    added logo to nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Nav.css'
-
+import logo from './logo.png';
 function Nav() {
   return (
     <div className='Navbox'>
-      <Link to="/">Wary</Link>
+      <Link to="/" > <img src={logo} alt="logo" /></Link>
+      {/* <Link to="/" style={{fontWeight: "700"}}>WARY</Link> */}
       <Link to="/">Report</Link>
       <Link to='/browse'>Browse</Link>
       <Link to='/communities'>Dashboard</Link>
